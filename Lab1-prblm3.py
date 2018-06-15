d8073f8206380d41972e3ebbd377aa766c825d91
Shruthi Pinnamwar <33705615+shruthipinnamwar@users.noreply.github.com>
2018-06-15 18:13:07 -0500

    Update Lab1 Problem 3

--- Lab1-prblm3.py
+++ Lab1-prblm3.py
@@ -1,4 +1,4 @@
-list1 = [-1, 0, 1, 2, -1, -4]
+list1 = [1, 3, 6, 2, -1, 2, 8, -2, 9]
 result = []
 list1.sort()
 end=len(list1)-1
@@ -11,7 +11,7 @@ for i in range(len(list1)-2):
         if (total > 0):
             end = end - 1
         if (total == 0):  # 0 is False in a boolean context
-            result.append([list1[i],list1[j],list1[end]])
+            result.append([list1[i],list1[start],list1[end]])
             start = start + 1  # increment l when we find a combination that works
 print(result)
 
