f7e6618dde256d24355333038b16e57e2c8f8546
Shruthi Pinnamwar <33705615+shruthipinnamwar@users.noreply.github.com>
2018-06-15 18:18:04 -0500

    Updates Lab1 Problem 1

--- Lab1-prblm1.py
+++ Lab1-prblm1.py
@@ -1,32 +1,39 @@
 
-def passwd_check(passwd):
+
+def Check(passwd):
    
-    SpecialSym=['$','@','#','%','!']
-    return_val=True
+    symbols=['$','@','#','%','!']
+    flag=True
     if len(passwd) < 6:
         print('the length of password must be at least 6 chars long')
-        return_val=False
+        flag=False
     if len(passwd) >16:
         print('the length of password must be not be greater than 8')
-        return_val=False
+        flag=False
     if not any(char.isdigit() for char in passwd):
         print('Password should contain at least one number')
-        return_val=False
+        flag=False
     if not any(char.isupper() for char in passwd):
         print('Password should contain at least one uppercase letter')
-        return_val=False
+        flag=False
     if not any(char.islower() for char in passwd):
         print('Password should contain at least one lowercase letter')
-        return_val=False
-    if not any(char in SpecialSym for char in passwd):
+        flag=False
+    if not any(char in symbols for char in passwd):
         print('the password should have at least one special character')
-        return_val=False
+        flag=False
     if return_val:
         print('Ok')
-    return return_val
+    return flag
 username = input('Enter Username : ')
 passwd = input('Enter Password : ')
-print(passwd_check(passwd))
+print(Check(passwd))
+
+
+
+
+
+
 
 
 
